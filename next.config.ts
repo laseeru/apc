f840276c850ed821c415daca0a26b9f3320af341
laseeru <blaseeru@gmail.com>
2026-07-12 18:56:24 -0400

    Fix Vercel build: remove outputFileTracingRoot causing ENOENT

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-import path from "path";
-
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   images: {
     unoptimized: true,
   },
